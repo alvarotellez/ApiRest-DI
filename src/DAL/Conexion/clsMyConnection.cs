d95281c370d546cd39e3a488f7ad232c6497ac00
Álvaro Téllez Hidalgo <alvaro.tellez.hidalgo@gmail.com>
2016-12-14 18:46:00 +0100

    Api Rest DI, terminada

---
+++ src/DAL/Conexion/clsMyConnection.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+
+
+namespace DAL.Conexion
+{
+    public class clsMyConnection
+    {
+
+        public string dataBase { get; set; }
+        public string user { get; set; }
+        public string pass { get; set; }
+        public string host { get; set; }
+
+
+
+        public clsMyConnection()
+        {
+            dataBase = "UWPSampleAlvaro";
+            user = "prueba";
+            pass = "iesnervion123.";
+            host = "uwpsamplealvaro1.database.windows.net";
+
+        }
+
+        public clsMyConnection(string host, string dataBase, string user, string pass)
+        {
+            this.dataBase = dataBase;
+            this.user = user;
+            this.pass = pass;
+            this.host = host;
+        }
+
+
+        //METODOS
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                
+                connection.ConnectionString = string.Format("server={0};database={1};uid={2};pwd={3};", host, dataBase, user, pass);
+               
+
+                connection.Open();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return connection;
+
+        }
+
+
+
+
+        public void closeConnection(ref SqlConnection connection)
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (InvalidOperationException)
+            {
+                throw; 
+            }
+            catch (Exception)
+            {
+            throw;
+            }
+        }
+
+
+    }
+
+}
